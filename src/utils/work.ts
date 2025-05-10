1e67c12f66a7ae67a5182839dca8fc9fc8ca6199
raghul2023 <raghul.ramesh002@gmail.com>
2025-05-10 17:54:42 +0530

    Update work.ts

--- src/utils/work.ts
+++ src/utils/work.ts
@@ -5,7 +5,7 @@ export const workExperience = [
       img: "/Zeilhoch.jpg", 
       date: "May 2024 - July 2024",
       link: "https://www.zielhoch.com/",
-      desc: "Conducted financial analysis and supported the preparation of detailed financial reports and presentations for clients. Collaborated with teams on financial planning and strategy development."
+      desc: "Conducted credit analysis and supported the preparation of detailed credit reports and presentations for clients. Collaborated with teams on financial planning and strategy development."
     },
     
   ];
